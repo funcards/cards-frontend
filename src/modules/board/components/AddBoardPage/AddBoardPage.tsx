436b094e2b58b9184f19c4d61bfccaccf5239644
Igor Agapie <igoragapie@gmail.com>
2021-12-02 19:10:35 +0200

    dev

--- src/modules/board/components/AddBoardPage/AddBoardPage.tsx
+++ src/modules/board/components/AddBoardPage/AddBoardPage.tsx
@@ -2,13 +2,15 @@ import React from 'react'
 import { useNavigate } from 'react-router-dom'
 import { Dialog } from '@reach/dialog'
 
+import * as classes from './AddBoardPage.module.scss'
+
 const AddBoardPage: React.FC = () => {
   const navigate = useNavigate()
 
   const onDismiss = () => navigate(-1)
 
   return (
-    <Dialog aria-labelledby="add-board" onDismiss={onDismiss}>
+    <Dialog aria-labelledby="add-board" onDismiss={onDismiss} className={classes.addBoard}>
       <p>Hello there. I am a dialog</p>
     </Dialog>
   )
