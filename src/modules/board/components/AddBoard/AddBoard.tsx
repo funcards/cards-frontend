db2bb43af7df944d0bd019cfebe3bf8e8f9ad9b0
Igor Agapie <igoragapie@gmail.com>
2021-12-04 04:25:24 +0200

    dev

--- src/modules/board/components/AddBoard/AddBoard.tsx
+++ src/modules/board/components/AddBoard/AddBoard.tsx
@@ -40,81 +40,83 @@ const AddBoard: React.FC = () => {
 
   return (
     <Dialog isOpen={newBoardIsOpen} initialFocusRef={nameRef} aria-labelledby="add-board" className={classes.addBoard}>
-      <h2 className={classes.addBoard__title}>
-        Create board
-        <button className={classes.addBoard__close} onClick={onDismiss}>
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            className={classes.addBoard__xIcon}
-            viewBox="0 0 24 24"
-            strokeWidth="2"
-            stroke="currentColor"
-            fill="none"
-            strokeLinecap="round"
-            strokeLinejoin="round"
-          >
-            <path stroke="none" d="M0 0h24v24H0z" fill="none" />
-            <line x1="18" y1="6" x2="6" y2="18" />
-            <line x1="6" y1="6" x2="18" y2="18" />
-          </svg>
-        </button>
-      </h2>
-      <form onSubmit={handleSubmit(onSubmit)} className="form">
-        <div className="form__row">
-          <Controller
-            name="color"
-            control={control}
-            rules={{ required: true }}
-            render={({ field }) => (
-              <div className={classes.addBoard__themes}>
-                {Object.values(Theme).map((t) => (
-                  <div
-                    key={t}
-                    data-theme={t}
-                    className={
-                      t === field.value
-                        ? `${classes.addBoard__theme} ${classes.addBoard__theme_active}`
-                        : classes.addBoard__theme
-                    }
-                    onClick={() => field.onChange(t)}
-                  >
-                    &nbsp;
-                  </div>
-                ))}
-              </div>
-            )}
-          />
-        </div>
-        <div className="form__row">
-          <input
-            type="text"
-            placeholder="Add board title"
-            className={errors.name ? 'form__input form__input_error' : 'form__input'}
-            {...rest}
-            ref={(e) => {
-              ref(e)
-              nameRef.current = e
-            }}
-          />
-          {errors.name && <strong className="form__error">{errors.name.message}</strong>}
-        </div>
-        <div className="form__row">
-          <textarea
-            placeholder="Description"
-            className={errors.description ? 'form__input form__input_error' : 'form__input'}
-            {...register('description', {
-              maxLength: 1000,
-            })}
-          />
-          {errors.description && <strong className="form__error">{errors.description.message}</strong>}
-        </div>
-        <div className="form__row">
-          <button type="submit" className="form__btn form__btn_end" disabled={disabled}>
-            {isLoading && <span role="loading" />}
-            Create board
+      <div className={classes.addBoard__wrapper}>
+        <h2 className={classes.addBoard__title}>
+          Create board
+          <button className={classes.addBoard__close} onClick={onDismiss}>
+            <svg
+              xmlns="http://www.w3.org/2000/svg"
+              className={classes.addBoard__xIcon}
+              viewBox="0 0 24 24"
+              strokeWidth="2"
+              stroke="currentColor"
+              fill="none"
+              strokeLinecap="round"
+              strokeLinejoin="round"
+            >
+              <path stroke="none" d="M0 0h24v24H0z" fill="none" />
+              <line x1="18" y1="6" x2="6" y2="18" />
+              <line x1="6" y1="6" x2="18" y2="18" />
+            </svg>
           </button>
-        </div>
-      </form>
+        </h2>
+        <form onSubmit={handleSubmit(onSubmit)} className="form">
+          <div className="form__row">
+            <Controller
+              name="color"
+              control={control}
+              rules={{ required: true }}
+              render={({ field }) => (
+                <div className={classes.addBoard__themes}>
+                  {Object.values(Theme).map((t) => (
+                    <div
+                      key={t}
+                      data-theme={t}
+                      className={
+                        t === field.value
+                          ? `${classes.addBoard__theme} ${classes.addBoard__theme_active}`
+                          : classes.addBoard__theme
+                      }
+                      onClick={() => field.onChange(t)}
+                    >
+                      &nbsp;
+                    </div>
+                  ))}
+                </div>
+              )}
+            />
+          </div>
+          <div className="form__row">
+            <input
+              type="text"
+              placeholder="Add board title"
+              className={errors.name ? 'form__input form__input_error' : 'form__input'}
+              {...rest}
+              ref={(e) => {
+                ref(e)
+                nameRef.current = e
+              }}
+            />
+            {errors.name && <strong className="form__error">{errors.name.message}</strong>}
+          </div>
+          <div className="form__row">
+            <textarea
+              placeholder="Description"
+              className={errors.description ? 'form__input form__input_error' : 'form__input'}
+              {...register('description', {
+                maxLength: 1000,
+              })}
+            />
+            {errors.description && <strong className="form__error">{errors.description.message}</strong>}
+          </div>
+          <div className="form__row">
+            <button type="submit" className="form__btn form__btn_end" disabled={disabled}>
+              {isLoading && <span role="loading" />}
+              Create board
+            </button>
+          </div>
+        </form>
+      </div>
     </Dialog>
   )
 }
