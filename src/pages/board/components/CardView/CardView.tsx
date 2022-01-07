338cb8330dd22aa2ac7f06a287d1ce2da54b3bf2
Igor Agapie <igoragapie@gmail.com>
2022-01-08 01:22:08 +0200

    ...

--- src/pages/board/components/CardView/CardView.tsx
+++ src/pages/board/components/CardView/CardView.tsx
@@ -1,9 +1,11 @@
 import React from 'react'
 import { Dialog } from '@reach/dialog'
+import { TiTimes } from 'react-icons/ti'
 
 import * as classes from './CardView.module.scss'
 
 import { Card } from '~src/store/board/board.types'
+import { Button } from '~src/ui-kit'
 
 export interface CardViewProps {
   card: Card
@@ -13,9 +15,19 @@ export interface CardViewProps {
 
 export const CardView: React.FC<CardViewProps> = ({ showCard, card, onClose }) => {
   return (
-    <Dialog isOpen={showCard} onDismiss={onClose} className={classes.viewCard}>
+    <Dialog isOpen={showCard} onDismiss={onClose} className={classes.viewCard} aria-labelledby={card.card_id}>
       <div className={classes.viewCard__wrapper}>
-        <h2>{card.name}</h2>
+        <div className={classes.viewCard__cover} />
+        <div className={classes.viewCard__header}>
+          {card.name}
+          <Button close={true} onClick={onClose}>
+            <TiTimes />
+          </Button>
+        </div>
+        <div className={classes.viewCard__body}>
+          <div className={classes.viewCard__main} />
+          <div className={classes.viewCard__sidebar} />
+        </div>
       </div>
     </Dialog>
   )
